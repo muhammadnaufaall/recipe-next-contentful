511908c645dad6f3320fa5eb91ede0db7ce244aa
Muhammad Naufal <muhamadnaufal429@gmail.com>
2022-07-01 19:52:36 +0700

    done

--- pages/index.js
+++ pages/index.js
@@ -20,12 +20,19 @@ export async function getStaticProps(){
 }
 
 export default function Recipes({recipes}) {
-  console.log(recipes);
   return (
     <div className="recipe-list">
       {recipes.map(recipe => (
         <RecipeCard key={recipe.sys.id} recipe={recipe} />
       ))}
+
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
     </div>
   )
 }
